b13a95c8c5257d03607385ea72a30df2402ce1b1
girvel <widauka@ya.ru>
2025-03-18 15:20:31 +0500

    1: POC

--- 1/main.py
+++ 1/main.py
@@ -1,14 +1,30 @@
-from typing import Annotated
+from contextlib import asynccontextmanager
+from typing import Annotated, TypeAlias
 
 import asyncpg
 import uvicorn
-from fastapi import APIRouter, FastAPI, Depends
+from fastapi import APIRouter, FastAPI, Depends, Request
 
 
-async def get_pg_connection() -> asyncpg.Connection: ...
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with asyncpg.create_pool(
+        host="db",
+        database="testdb",
+        user="postgres",
+        password="postgres",
+    ) as pool:  # TODO! hardcode
+        yield {"db_pool": pool}
 
 
-async def get_db_version(conn: Annotated[asyncpg.Connection, Depends(get_pg_connection)]):
+async def get_pg_connection(request: Request) -> asyncpg.Connection:
+    async with request.state.db_pool.acquire() as conn:
+        yield conn
+
+PgConnection: TypeAlias = Annotated[asyncpg.Connection, Depends(get_pg_connection)]
+
+
+async def get_db_version(conn: PgConnection):
     return await conn.fetchval("SELECT version()")
 
 
@@ -19,10 +35,10 @@ def register_routes(app: FastAPI):
 
 
 def create_app() -> FastAPI:
-    app = FastAPI(title="e-Comet")
+    app = FastAPI(title="e-Comet", lifespan=lifespan)
     register_routes(app)
     return app
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:create_app", factory=True)
+    uvicorn.run("main:create_app", factory=True, host="0.0.0.0")  # TODO! 0.0.0.0 is probably not hardcode, right?
