310104b2e66ecf650ca9a2ad997cfce2e3fe3632
girvel <widauka@ya.ru>
2026-03-10 12:13:12 +0500

    Fix #4

--- 4/query.sql
+++ 4/query.sql
@@ -1,9 +1,15 @@
-SELECT phrase, groupArray(t) views_by_hour FROM (
-    SELECT phrase, tuple(hour, sum(views)) AS t
+SELECT phrase, groupArray(tuple(hour, views_in_hour)) AS views_by_hour
+FROM (
+    SELECT 
+        phrase, 
+        toHour(dt) AS hour,
+        max(views) AS max_views,
+        -- Subtract the previous hour's max cumulative views to get the delta for this hour
+        max_views - lagInFrame(max_views, 1, min(views)) OVER (PARTITION BY phrase ORDER BY hour) AS views_in_hour
     FROM phrases_views
-    WHERE campaign_id = 1111111
-    AND toDate(dt) = toDate('2025-01-01') /* let's pretend today is 2025-01-01 */
-    GROUP BY toHour(dt) AS hour, phrase
+    WHERE campaign_id = 1111111 AND toDate(dt) = '2025-01-01'
+    GROUP BY phrase, toHour(dt) AS hour
     ORDER BY hour DESCENDING
 )
+WHERE views_in_hour > 0  -- Filter out 0-view hours to match the exact output requested
 GROUP BY phrase;
